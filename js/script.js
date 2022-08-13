c02d0a7fafb436aa5068cabbebd0dce7e95cf395
developersview <pcslg1998@gmail.com>
2022-08-13 23:16:35 +0530

    implementing reset button functionality

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
 'use strict';
-/* console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉Correct Number';
-document.querySelector('.number').textContent = 17;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value); */
 
 let score = 20;
-const secret = Math.trunc(Math.random() * 20) + 1;
+let secret = Math.trunc(Math.random() * 20) + 1;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -20,6 +13,7 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.message').textContent = '🎉Correct Number';
         document.querySelector('.number').textContent = secret;
+        document.querySelector('.number').style.width = '30rem';
     } else if (guess > secret) {
         // when guess is too high
         if (score > 1) {
@@ -41,4 +35,14 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.score').textContent = 0;
         }
     }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secret = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'Start Guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.guess').value = '';
 });
